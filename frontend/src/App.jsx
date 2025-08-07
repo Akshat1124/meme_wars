b7c4eaf0bf59c50fec92797df93d81e9115b6d5c
Harsh Sharma <harsh.sharma.8986@gmail.com>
2025-08-08 02:39:53 +0530

    frontend init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+
+  return (
+   <div className="min-h-screen bg-black text-white text-4xl flex items-center justify-center ">
+      <h1>Hello</h1>
+   </div>
+  )
+}
+
+
