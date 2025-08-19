e10d120cd9a187c5fb803fa25ed9b4c72da11c87
AKSHAT SRIVASTAVA <akshatsrivastava1124@gmail.com>
2025-08-19 23:45:57 +0530

    chore: update config or code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <-- Yeh line sabse zaroori hai
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
